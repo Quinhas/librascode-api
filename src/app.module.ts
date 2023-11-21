4013e01b4c8267b71f5d406529aa21f7f83db19e
Lucas Santana <ln.santana08@gmail.com>
2023-11-21 14:31:14 -0300

    feat: deploy vercel

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
-import { MulterConfigModule } from '@infra/modules/multer.module';
 
 @Module({
-  imports: [MulterConfigModule, HttpModule, DatabaseModule],
+  imports: [HttpModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
